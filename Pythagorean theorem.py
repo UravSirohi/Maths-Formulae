0b56aa45b10d4196c095af863a321bb3e173450e
Urav Sirohi <urav.sirohi@outlook.com>
2020-06-21 16:18:18 +0100

    new fromulaes

---
+++ Pythagorean theorem.py
@@ -0,0 +1,59 @@
+import operator
+
+
+def main():
+    which_function = input('''Which side's value would you like to work out?
+>>>''')
+    if which_function == 'a':
+        find_value_a()
+    elif which_function == 'b':
+        find_value_b()
+    elif which_function == 'h':
+        find_value_c()
+    else:
+        print('''Input options are either a(adjacent), b(opposite) or h(hypotenuse)''')
+
+
+def find_value_a():
+    h = float(input('Value of h: '))
+    b = float(input('Value of b: '))
+    h_squared = operator.pow(h, 2)
+    b_squared = operator.pow(b, 2)
+    a_squared = operator.sub(h_squared, b_squared)
+    if a_squared < 0:
+        print('There is no square root of negative numbers')
+        raise ZeroDivisionError
+    a = operator.pow(a_squared, 0.5)
+    print(f'Value of a is {a}')
+    main()
+
+
+def find_value_b():
+    h = float(input('Value of h: '))
+    a = float(input('Value of a: '))
+    h_squared = operator.pow(h, 2)
+    a_squared = operator.pow(a, 2)
+    b_squared = operator.sub(h_squared, a_squared)
+    if b_squared < 0:
+        print('There is no square root of negative numbers')
+        raise ZeroDivisionError
+    b = operator.pow(b_squared, 0.5)
+    print(f'Value of b is {b}')
+    main()
+
+
+def find_value_c():
+    b = float(input('Value of b: '))
+    a = float(input('Value of a: '))
+    b_squared = operator.pow(b, 2)
+    a_squared = operator.pow(a, 2)
+    h_squared = operator.add(b_squared, a_squared)
+    if h_squared < 0:
+        print('There is no square root of negative numbers')
+        raise ZeroDivisionError
+    h = operator.pow(h_squared, 0.5)
+    print(f'Value of h is {h}')
+    main()
+
+
+main()
--- Quadratic Equation.py
+++ Quadratic Equation.py
@@ -2,20 +2,29 @@ import operator
 
 
 def main():
-    print('''Please enter the quadratic by giving these values:
-        ax^2+bx+c=y''')
-    a = int(input('Value of a: '))
-    b = int(input('Value of b: '))
-    c = int(input('Value of c: '))
-    in_square_root_value_1 = operator.mul(-4, operator.mul(a, c))
-    in_square_root_value = operator.sub(operator.pow(b, 2), in_square_root_value_1)
-    square_root_value = operator.pow(in_square_root_value, 0.5)
-    ans_1_working_out_1 = operator.sub(operator.mul(b, -1), square_root_value)
-    ans_2_working_out_1 = operator.add(operator.mul(b, -1), square_root_value)
-    bottom_value = operator.mul(2, a)
-    ans_1 = operator.truediv(ans_1_working_out_1, bottom_value)
-    ans_2 = operator.truediv(ans_2_working_out_1, bottom_value)
-    print(f'''x = {ans_1} or
+    while True:
+        print('''Please enter the quadratic by giving these values:
+            ax^2+bx+c=y''')
+        a = float(input('Value of a: '))
+        b = float(input('Value of b: '))
+        c = float(input('Value of c: '))
+        in_square_root_value_1 = operator.mul(-4, operator.mul(a, c))
+        in_square_root_value = operator.add(operator.pow(b, 2), in_square_root_value_1)
+        if in_square_root_value < 0:
+            print('There is no square root of negative numbers')
+            raise ZeroDivisionError
+        else:
+            square_root_value = operator.pow(in_square_root_value, 0.5)
+            ans_1_working_out_1 = operator.sub(operator.mul(b, -1), square_root_value)
+            ans_2_working_out_1 = operator.add(operator.mul(b, -1), square_root_value)
+            bottom_value = operator.mul(2, a)
+            ans_1 = operator.truediv(ans_1_working_out_1, bottom_value)
+            ans_2 = operator.truediv(ans_2_working_out_1, bottom_value)
+            if ans_2.is_integer():
+                ans_2 = int(ans_2)
+            if ans_1.is_integer():
+                ans_1 = int(ans_1)
+            print(f'''x = {ans_1} or
 x = {ans_2}''')
 
 
